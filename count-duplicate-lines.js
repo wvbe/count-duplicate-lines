1ed1b3fc2c1787720a4a89f45da6e0514f4c9fb3
wvbe <wybe@x-54.com>
2020-08-23 09:53:03 +0100

    Simplify and annotate code a bit

--- count-duplicate-lines.js
+++ count-duplicate-lines.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const KEY_WIDTH = 10;
+const RULER_TEXT = '--------------------------------------';
+
+// Promise the input piped into this program
 async function getStreamedInputData() {
 	if (process.stdin.isTTY) {
 		throw new Error('Process STDIN is not TTY');
@@ -12,43 +16,40 @@ async function getStreamedInputData() {
 		}
 	});
 
-	return new Promise(resolve => process.stdin.on('end', () => resolve(data.split('\n'))));
+	return new Promise((resolve) => process.stdin.on('end', () => resolve(data.split('\n'))));
 }
 
-function lpad(str, len, padChar) {
-	return str.length >= len
-		? str
-		: Array.from(new Array(len - str.length))
-				.map(() => padChar)
-				.join('') + str;
+// Print two columns, where the left column is 10 characters and aligned to the right
+function log(left, right) {
+	console.log(
+		left.length >= KEY_WIDTH ? left : ' '.repeat(KEY_WIDTH - left.length) + left + ' | ' + right
+	);
 }
 
+// Porcelain mode includes less formatting text, making it easier to parse by other software
 const hasPorcelain = process.argv.includes('-p') || process.argv.includes('--porcelain');
+
+// Run the script
 (async () => {
-	const lines = (await getStreamedInputData()).filter(line => !!line);
+	const lines = (await getStreamedInputData()).filter((line) => !!line);
 
 	const totals = lines.reduce((tot, line) => {
 		tot[line] = (tot[line] || 0) + 1;
 		return tot;
 	}, {});
 
-	const totalsAsArray = Object.keys(totals)
-		.map(line => [totals[line], line])
-		.sort(([totalA, _lineA], [totalB, _lineB]) => totalB - totalA);
+	const sorted = Object.keys(totals)
+		.map((line) => [totals[line], line])
+		.sort(([totalA], [totalB]) => totalB - totalA);
 
-	if (!hasPorcelain) {
-		console.log(lpad('COUNT', 10, ' ') + ' | LINE');
-		console.log('--------------------------------------');
+	if (hasPorcelain) {
+		sorted.forEach(([total, line]) => console.log(total + '\t' + line));
+	} else {
+		log('COUNT', 'LINE');
+		console.log(RULER_TEXT);
+		sorted.forEach(([total, line]) => log(String(total), line));
+		console.log(RULER_TEXT);
+		log(String(lines.length), 'TOTAL LINES');
+		log(String(Object.keys(totals).length), 'UNIQUE LINES');
 	}
-	totalsAsArray.forEach(([total, line]) =>
-		hasPorcelain
-			? console.log(total + '\t' + line)
-			: console.log(lpad(total + '', 10, ' ') + ' | ' + line)
-	);
-
-	if (!hasPorcelain) {
-		console.log('--------------------------------------');
-		console.log(lpad(lines.length + '', 10, ' ') + ' | TOTAL LINES');
-		console.log(lpad(Object.keys(totals).length + '', 10, ' ') + ' | UNIQUE LINES');
-	}
-})().catch(error => console.error(error.stack || error));
+})().catch((error) => console.error(error.stack || error));
